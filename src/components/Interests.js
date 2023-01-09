0da37fb297601a2845ea2bc0f76a79937fb862b4
newengood <04-scones.card@newengood.com>
2023-01-08 22:50:41 -0800

    replaced wav with mp3

--- src/components/Interests.js
+++ src/components/Interests.js
@@ -1,11 +1,11 @@
 import Foster from '../docs/Foster.png';
 import WhiffRates from '../docs/WhiffRates.pdf';
-import Reggae from '../docs/Reggae.wav'
-import EDM from '../docs/EDM.wav'
-import SocialMediaAd from '../docs/SocialMediaAd.wav'
-import HipHop from '../docs/HipHop.wav'
-import Indie from '../docs/Indie.wav'
-import Soundtrack from '../docs/Soundtrack.wav'
+import Reggae from '../docs/Reggae.mp3'
+import EDM from '../docs/EDM.mp3'
+import SocialMediaAd from '../docs/SocialMediaAd.mp3'
+import HipHop from '../docs/HipHop.mp3'
+import Indie from '../docs/Indie.mp3'
+import Soundtrack from '../docs/Soundtrack.mp3'
 
 // Renders Interests Tab
 
@@ -34,27 +34,27 @@ const Interests = () => {
           <div class='card-body'>
           <h3>Reggae</h3>
           <audio id="player"controls>
-            <source src={Reggae} type="audio/wav"></source>
+            <source src={Reggae} type="audio/mp3"></source>
           </audio>
           <h3>EDM</h3>
           <audio id="player"controls>
-            <source src={EDM} type="audio/wav"></source>
+            <source src={EDM} type="audio/mp3"></source>
           </audio>
           <h3>Hip Hop</h3>
           <audio id="player"controls>
-            <source src={HipHop} type="audio/wav"></source>
+            <source src={HipHop} type="audio/mp3"></source>
           </audio>
           <h3>Soundtrack</h3>
           <audio id="player"controls>
-            <source src={Soundtrack} type="audio/wav"></source>
+            <source src={Soundtrack} type="audio/mp3"></source>
           </audio>
           <h3>Indie</h3>
           <audio id="player" controls>
-            <source src={Indie} type="audio/wav"></source>
+            <source src={Indie} type="audio/mp3"></source>
           </audio>
           <h3>Social Media Ad</h3>
           <audio id="player"controls>
-            <source src={SocialMediaAd} type="audio/wav"></source>
+            <source src={SocialMediaAd} type="audio/mp3"></source>
           </audio>
           </div>
         </div>
