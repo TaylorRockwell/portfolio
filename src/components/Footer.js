4bb2e41cffeffb4b6926d8a8050aaf72caf35539
Taylor Anderson <taylor@newengood.com>
2023-05-28 23:22:06 -0700

    updated email address

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer class="w-100 mt-auto bg-secondary p-4">
       <div class="container text-center mb-5">
       <a href="https://github.com/TaylorRockwell">Github | </a>
-      <a href="mailto:contact@taylorrockwell.com">Email</a>
+      <a href="mailto:info@taylorrockwell.com">Email</a>
       </div>
     </footer>
   );
