016d191d31a57c69070a2a24f64c6896e93bf50b
Taylor Anderson <taylor@newengood.com>
2023-10-11 22:48:16 -0700

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,9 @@ const Footer = () => {
     <footer class="w-100 mt-auto bg-secondary p-4">
       <div class="container text-center mb-5">
       <a href="https://github.com/TaylorRockwell">Github | </a>
-      <a href="mailto:info@taylorrockwell.com">Email</a>
+      <a href="mailto:info@taylorrockwell.com">Email | </a>
+    <a href="www.linkedin.com/in/taylor-rockwell-anderson-568531231">LinkedIn</a>
+    
       </div>
     </footer>
   );
